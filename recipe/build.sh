f4f1d98503680d1140d1a9f9cf9589a3fbe513ea
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-04-29 09:40:30 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# Mac Os X does not rename the file as part of the patch 
+if test -f makefile.lnx_ifort; then
+    mv makefile.lnx_ifort makefile
+fi
+
+# Validate folder structure 
 ls
 cat makefile
+
+# Compile 
 make bader
 mkdir -p ${PREFIX}/bin
 cp bader ${PREFIX}/bin/bader
